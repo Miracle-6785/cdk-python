8f862a8018a10ee465dd9bd3f1fcd1e133e01155
Thang Viet Nguyen <miracleiztb@gmail.com>
2025-05-03 17:54:18 +0700

    first commit

---
+++ cdk_workshop/cdk_workshop_stack.py
@@ -0,0 +1,33 @@
+from aws_cdk import Duration, Stack
+from aws_cdk import aws_apigateway as apigw
+from aws_cdk import aws_iam as iam
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_sns as sns
+from aws_cdk import aws_sns_subscriptions as subs
+from aws_cdk import aws_sqs as sqs
+from cdk_dynamo_table_view import TableViewer
+from constructs import Construct
+
+from cdk_workshop.hitcounter import HitCounter
+
+
+class CdkWorkshopStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        my_lambda = _lambda.Function(
+            self,
+            "HelloHandler",
+            runtime=_lambda.Runtime.PYTHON_3_13,
+            code=_lambda.Code.from_asset("lambda"),
+            handler="hello.handler",
+        )
+
+        hello_with_counter = HitCounter(self, "HelloHitCounter", downstream=my_lambda)
+
+        apigw.LambdaRestApi(self, "gwHello", handler=hello_with_counter.handler)
+
+        TableViewer(
+            self, "ViewHitCounter", title="Hello Hits", table=hello_with_counter.table
+        )
---
+++ cdk_workshop/pipeline_stack.py
@@ -0,0 +1,23 @@
+from aws_cdk import Stack
+from aws_cdk import pipelines as pipelines
+from constructs import Construct
+
+# arn:aws:codeconnections:ap-southeast-1:637423223528:connection/2d86ea02-e6cf-4f33-893a-714c7818686c
+
+class WorkshopPipelineStack(Stack):
+
+    def __init__(self, scope: Construct, id: str, codeStarArn: str ,**kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        pipeline = pipelines.CodePipeline(
+            self,
+            "Pipeline",
+            synth=pipelines.ShellStep(
+                "Synth",
+                input=pipelines.CodePipelineSource.connection(
+                    repo_string=""
+                    branch="main",
+                    connection_arn="arn:aws:codeconnections:ap-southeast-1:637423223528:connection/2d86ea02-e6cf-4f33-893a-714c7818686c"
+                )
+            )
+        )
